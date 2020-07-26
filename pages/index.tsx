5e829778811e5897c8afcb5da8ef5fdaccdd11b4
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-07-26 14:11:12 -0500

    reworked global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,31 +11,6 @@ function Home() {
       </Head>
 
       <Header />
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        .container {
-          margin-right: auto; /* 1 */
-          margin-left: auto; /* 1 */
-
-          max-width: 960px; /* 2 */
-
-          padding-right: 10px; /* 3 */
-          padding-left: 10px; /* 3 */
-        }
-      `}</style>
     </div>
   );
 }
