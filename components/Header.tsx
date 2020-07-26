6fac459072f2d185f81c902d5d0dfdb5f1ed7447
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-07-26 14:08:13 -0500

    removed debugging changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,25 @@
 function Header() {
   return (
     <div className="container">
-      <h1 className="logo2">Oblivious</h1>
-      <p className="tagLine2">
+      <h1 className="logo">Oblivious</h1>
+      <p className="tagLine">
         Follow a few cool people without actually being on Twitter.
       </p>
       <br />
 
-      <style jsx global>{`
-        .logo2 {
+      <style jsx>{`
+        .logo {
           font-size: 80px;
           margin-bottom: 5px;
         }
 
-        .tagLine2 {
+        .tagLine {
           margin-top: 0;
           color: #656464;
         }
 
         @media only screen and (max-width: 768px) {
-          .logo2 {
+          .logo {
             font-size: 50px;
             margin-bottom: 5px;
           }
